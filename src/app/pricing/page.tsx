fce6b535d9435dfd5cc5bb9003ce34d5be403269
Suzy Soares <suzyscampos12@gmail.com>
2025-08-16 07:59:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from 'react';
 import { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
