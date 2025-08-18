d7789063829957670814ecae569ee5e31625a335
Suzy Soares <suzyscampos12@gmail.com>
2025-08-18 02:10:39 +0000

    aonde é o erro?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,15 @@
-
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyAFrH7XH1JtVUGB6RWnNOwC4izUTmIKBgg",
+    authDomain: "lar-seguro-76fan.firebaseapp.com",
+    projectId: "lar-seguro-76fan",
+    storageBucket: "lar-seguro-76fan.firebasestorage.app",
+    messagingSenderId: "951306744726",
+    appId: "1:951306744726:web:d57961e8dc59a8648fe3b7",
 };
 
 // Initialize Firebase
