c937fc0f3512533db3e305144ce7c70af3d47cdc
Suzy Soares <suzyscampos12@gmail.com>
2025-08-19 00:49:57 +0000

    reduza em 50% o tamanho dos gráficos

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -303,7 +303,7 @@ function ReportChart({ title, data, dataKey }: { title: string, data: any[], dat
       <CardContent className="grid gap-6">
         <div>
           <h3 className="font-semibold mb-2 text-sm">Receita</h3>
-           <ChartContainer config={chartConfig} className="min-h-[120px] w-full">
+           <ChartContainer config={chartConfig} className="min-h-[60px] w-full">
             <BarChart accessibilityLayer data={data}>
               <CartesianGrid vertical={false} />
               <XAxis
@@ -329,7 +329,7 @@ function ReportChart({ title, data, dataKey }: { title: string, data: any[], dat
         </div>
         <div>
           <h3 className="font-semibold mb-2 text-sm">Agendamentos</h3>
-           <ChartContainer config={chartConfig} className="min-h-[120px] w-full">
+           <ChartContainer config={chartConfig} className="min-h-[60px] w-full">
             <RechartsLineChart accessibilityLayer data={data}>
               <CartesianGrid vertical={false} />
               <XAxis
@@ -360,3 +360,4 @@ function ReportChart({ title, data, dataKey }: { title: string, data: any[], dat
     
 
     
+
