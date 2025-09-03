c59489ea962d3615f0e8046688027cfeb24f538a
Suzy Soares <suzyscampos12@gmail.com>
2025-09-03 23:26:40 +0000

    https://postimg.cc/0b0JsBC6 faça o ajuste da logo que esta no canto supe

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function Logo({ className, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) {
   return (
     <Image
-      src="https://i.postimg.cc/mD4p2yDs/logo-oficial-2.png"
+      src="https://i.postimg.cc/0j3Zz3gT/logo-oficial-2.png"
       alt="ServirLar Logo"
       width={180}
       height={40}
