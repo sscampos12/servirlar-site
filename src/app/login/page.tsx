4850dbf46766356b7df10798c763471f83b9ace9
Suzy Soares <suzyscampos12@gmail.com>
2025-09-06 00:34:49 +0000

    PERFEITO

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 import { Suspense } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { LoginForm } from '@/components/login-form';
